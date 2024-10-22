b993727b379ce6bfef47ab8f1f714966138f39d3
MarcosFerrandiz <marcos149ft@gmail.com>
2024-10-22 15:39:20 +0200

    Update Ejercicio1.java

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -1,1 +1,16 @@
+package com.marcosferrandiz.tema03.metodos;
 
+
+public class Ejercicio1 {
+    public static int suma (int num1, int num2){
+        return num1+num2;
+    }
+
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 4;
+
+        int resultadoSuma = suma(num1,num2);
+        System.out.println(resultadoSuma);
+    }
+}
