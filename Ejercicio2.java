7cb2c7c5225f926857450381df5499b26efc028a
MarcosFerrandiz <marcos149ft@gmail.com>
2024-10-22 15:39:41 +0200

    Update Ejercicio2.java

--- Ejercicio2.java
+++ Ejercicio2.java
@@ -1,1 +1,27 @@
+package com.marcosferrandiz.tema03.metodos;
 
+import java.util.Scanner;
+
+public class Ejercicio2 {
+    public class Mates{
+        public static double area (double radio){
+            return Math.PI * radio * radio;
+        }
+        public static double longitud (double radio){
+            return (radio*2)* Math.PI;
+        }
+    }
+
+    public static void main(String[] args) {
+        double radio;
+        double areaTotal;
+        double longitudTotal;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Indique el radio para calcular el area y la longitud");
+        radio = Double.parseDouble(input.nextLine());
+        areaTotal = Mates.area(radio);
+        longitudTotal = Mates.longitud(radio);
+        System.out.printf("El area total del circulo es %.2f cm2 por ejemplo, y la longitud es de %.2f cm por ejemplo también", areaTotal, longitudTotal);
+
+    }
+}
